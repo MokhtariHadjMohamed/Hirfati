8e912735579e53776c3ca3e74e47ac9d2989c2a1
7adj_Mo7amed <7adj.mo7amed@gmail.com>
2023-09-12 15:46:45 +0100

    the first login frame design ui with some change to be responsive

--- app/src/main/java/com/hadjmohamed/hirfati/MainActivity.java
+++ app/src/main/java/com/hadjmohamed/hirfati/MainActivity.java
@@ -2,6 +2,7 @@ package com.hadjmohamed.hirfati;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startActivity(new Intent(MainActivity.this, LogIn.class));
     }
 }
