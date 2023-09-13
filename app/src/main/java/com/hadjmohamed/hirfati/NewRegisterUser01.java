1cceed6a4cd83a3c2fc26f627691c5b4bd41d815
7adj_Mo7amed <7adj.mo7amed@gmail.com>
2023-09-13 17:15:43 +0100

    add wilaya and create register page

---
+++ app/src/main/java/com/hadjmohamed/hirfati/NewRegisterUser01.java
@@ -0,0 +1,14 @@
+package com.hadjmohamed.hirfati;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewRegisterUser01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_register_user01);
+    }
+}
