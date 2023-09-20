f42e3836b210cb7732dd43d2493eca1cbc3aff41
7adj_Mo7amed <7adj.mo7amed@gmail.com>
2023-09-20 15:44:38 +0100

    create most of page admin

---
+++ app/src/main/java/com/hadjmohamed/hirfati/Admin/AdminReport.java
@@ -0,0 +1,16 @@
+package com.hadjmohamed.hirfati.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hadjmohamed.hirfati.R;
+
+public class AdminReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_report);
+    }
+}
