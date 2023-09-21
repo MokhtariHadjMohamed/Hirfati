b13ab5b0a86a2ad3caa11eb1a9661e6e1b942b65
7adj_Mo7amed <7adj.mo7amed@gmail.com>
2023-09-21 15:18:14 +0100

    add last page of admin and some layout for recycle view

---
+++ app/src/main/java/com/hadjmohamed/hirfati/Admin/AdminReportInfo.java
@@ -0,0 +1,16 @@
+package com.hadjmohamed.hirfati.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hadjmohamed.hirfati.R;
+
+public class AdminReportInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_report_info);
+    }
+}
